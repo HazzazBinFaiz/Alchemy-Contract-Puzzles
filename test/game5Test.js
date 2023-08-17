6da5cd02385af491f6315c89e4b4cea1948d1710
MD. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2023-08-17 07:53:05 +0600

    Solve game5

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,5 +1,6 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
@@ -10,10 +11,17 @@ describe('Game5', function () {
   }
   it('should be a winner', async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
-
     // good luck
+    const privKey = '0x8cec9e1ad414ba90c3eaa96ac3c781f64aa4ea7b63d3fe940e6dc1b9247f54b5';
 
-    await game.win();
+    const wallet = new ethers.Wallet(privKey, ethers.provider);
+
+    await ethers.provider.getSigner(0).sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther('1')
+    })
+
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
